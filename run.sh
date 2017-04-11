ad9d37c7a7d50ea9f69302b8734db2726b5a42a4
dhrp <t.peskens@nerdalize.com>
2017-04-11 22:18:09 +0200

    moved to use /workdir

--- run.sh
+++ run.sh
@@ -13,9 +13,17 @@ fi
 
 # echo "starting psspred with input:"
 cat $TMP_FILE
+echo ""
+
+# clean up workdir
+rm -rf /workdir
+
+# create new one
+mkdir -p /workdir
+cd /workdir
 
 /library/yzhang/PSSpred/PSSpred.pl $TMP_FILE
 
-cat "/seq.dat.ss" |  grep -v "coil  helix  beta" | awk -v OFS="\n" '{if($3 == "C") print "L:"$4;if($3 == "H") print "H:"$5; if($3 == "E") print "E:"$6;}' | tr "\n" "," > /tmp/output.txt
+cat "seq.dat.ss" |  grep -v "coil  helix  beta" | awk -v OFS="\n" '{if($3 == "C") print "L:"$4;if($3 == "H") print "H:"$5; if($3 == "E") print "E:"$6;}' | tr "\n" "," > /workdir/output.txt
 
 echo "process complete"
